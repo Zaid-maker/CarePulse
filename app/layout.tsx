192335b2968723228d1a79578d514b767de878db
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-14 11:32:54 +0000

    init dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,12 +26,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={
-        cn(
-          'min-h-screen bg-dark-300 font-sans antialiased',
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
           fontSans.variable
-        )
-      }>{children}</body>
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
