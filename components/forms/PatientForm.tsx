a470d1e3b0d243f56f14f4d928630821e3787238
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-14 11:48:54 +0000

    update

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const PatientForm = () => {
   return (
-    <div>PatientForm</div>
+    <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Get started with appointments.</p>
+        </section>
   )
 }
 
