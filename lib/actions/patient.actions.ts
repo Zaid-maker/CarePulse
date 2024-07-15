ec2e271e81cb3ab789620e9bc43cf8596604e009
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-15 17:18:04 +0000

    newUser server action

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  // databases,
+  // storage,
+  users,
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+
+    return parseStringify(newUser);
+  } catch (error: any) {
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
+
+      return existingUser.users[0];
+    }
+
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
