fa319267c8ef477bd5f4088d530fd6e8d374d981
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-15 11:14:14 +0000

    admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+        <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+
+      <main className="admin-main">
+      <section className="w-full space-y-4">
+      <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
+      </section>
+
+      <section className="admin-stat">
+        StatCard
+      </section>
+
+      DataTable
+      </main>
+    </div>
+  )
+}
+
+export default AdminPage
