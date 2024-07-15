17662bd1c64541d4ad3e1ad75181f6e3dfd27611
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-15 17:38:51 +0000

    update style

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -51,7 +51,7 @@ const PatientForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="mb-12 space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
           <p className="text-dark-700">Get started with appointments.</p>
