189b27f94ae2d9e727b8e2820432ef97c5af10d7
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-09 11:08:42 +0000

    update appointment

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { SelectItem } from "@/components/ui/select";
+import { Doctors } from "@/constants";
+// import {
+//   createAppointment,
+//   updateAppointment,
+// } from "@/lib/actions/appointment.actions";
+import { getAppointmentSchema } from "@/lib/validation";
 import { Appointment } from "@/types/appwrite.types";
-import React, { Dispatch, SetStateAction } from "react";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { Form } from "../ui/form";
 
 export const AppointmentForm = ({
   userId,
@@ -14,5 +35,25 @@ export const AppointmentForm = ({
   appointment?: Appointment;
   setOpen?: Dispatch<SetStateAction<boolean>>;
 }) => {
-  return <div>AppointmentForm</div>;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const AppointmentFormValidation = getAppointmentSchema(type);
+
+  const form = useForm<z.infer<typeof AppointmentFormValidation>>({});
+
+  return (
+    <Form {...form}>
+      <form className="flex-1 space-y-6">
+        {type === "create" && (
+          <section className="mb-12 space-y-4">
+            <h1 className="header">New Appointment</h1>
+            <p className="text-dark-700">
+              Request a new appointment in 10 seconds.
+            </p>
+          </section>
+        )}
+      </form>
+    </Form>
+  );
 };
