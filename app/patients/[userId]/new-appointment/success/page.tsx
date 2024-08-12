7d7a3036fbe48604047c1cba8594521e3f3d1801
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-12 19:11:19 +0000

    create success page

---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RequestSuccess = async ({
+    searchParams,
+    params: { userId }
+}: SearchParamProps) => {
+  return (
+    <div className='flex h-screen max-h-screen px-[5%]'>RequestSuccess</div>
+  )
+}
+
+export default RequestSuccess
