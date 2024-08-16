97ae0bbeb75f0ce82cac4846ae1fdb0207a07342
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-16 15:22:22 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && "PassKeyModek"}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -37,4 +40,6 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+
+export default Home;
