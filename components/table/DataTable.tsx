19d9b1a978c7179aa8555e312677d1af49bcd4c0
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-16 17:34:33 +0000

    changes

--- components/table/DataTable.tsx
+++ components/table/DataTable.tsx
@@ -1,9 +1,27 @@
-'use client'
+"use client";
 
-import React from 'react'
+import {
+  getPaginationRowModel,
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { decryptKey } from "@/lib/utils";
 
 export const DataTable = () => {
-  return (
-    <div>DataTable</div>
-  )
-}
+  return <div>DataTable</div>;
+};
