48c1bbbe97da5bea73be319e3af0625c7f4a7c53
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-08-16 15:34:13 +0000

    admin passkey

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,9 +1,58 @@
-'use client'
+"use client";
 
-import React from 'react'
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { decryptKey, encryptKey } from "@/lib/utils";
 
 export const PasskeyModal = () => {
   return (
-    <div>PasskeyModal</div>
-  )
-}
+    <AlertDialog open>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              //   onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access the admin page, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div>
+          <InputOTP maxLength={6}>
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
