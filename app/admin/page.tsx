1a6f07880afd829ef9d002b91fc7f9cd95fb13ce
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-10-04 14:30:00 +0000

    StatCard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import { StatCard } from "@/components/StatCard";
 import { columns } from "@/components/table/columns";
 import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
@@ -31,7 +32,26 @@ const AdminPage = async () => {
           </p>
         </section>
 
-        <section className="admin-stat">StatCard</section>
+        <section className="admin-stat">
+        <StatCard
+            type="appointments"
+            count={appointments.scheduledCount}
+            label="Scheduled appointments"
+            icon={"/assets/icons/appointments.svg"}
+          />
+          <StatCard
+            type="pending"
+            count={appointments.pendingCount}
+            label="Pending appointments"
+            icon={"/assets/icons/pending.svg"}
+          />
+          <StatCard
+            type="cancelled"
+            count={appointments.cancelledCount}
+            label="Cancelled appointments"
+            icon={"/assets/icons/cancelled.svg"}
+          />
+        </section>
 
         <DataTable columns={columns} data={appointments.documents} />
       </main>
